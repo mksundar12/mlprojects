3178faae895a7aaa398801a10792662eca9d205f
Meenakshi <mksundar00@gmail.com>
2025-10-04 22:49:45 +0530

    setup

---
+++ src/exception.py
@@ -0,0 +1,7 @@
+import sys
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    line_number = exc_tb.tb_lineno
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = f"Error occurred in script: {file_name} at line number: {line_number} error message: {str(error)}"
+    return error_message
